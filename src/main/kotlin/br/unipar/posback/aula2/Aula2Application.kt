7ac36ed274f9d23bd1b9697691536db75ba4148d
Djonathan Barros <djonathan.barros@gmail.com>
2021-09-25 09:19:22 -0300

    carregamento inicial
    (cherry picked from commit a2a55079753f06167c683fd42c46c0f7beb1dc9e)

---
+++ src/main/kotlin/br/unipar/posback/aula2/Aula2Application.kt
@@ -0,0 +1,11 @@
+package br.unipar.posback.aula2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Aula2Application
+
+fun main(args: Array<String>) {
+    runApplication<Aula2Application>(*args)
+}
